dbcf464e104e08435f900bd3fd9510af39ccaf49
manriquez02cesar <manriquez02cesar@gmail.com>
2026-03-27 23:24:25 -0600

    Se agregaron las variables a utilizar

--- src/MayorMenor.java
+++ src/MayorMenor.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class MayorMenor {
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
 
+        //Se ingresara un primer elemento el cual se va a comparar con otros 2 elementos
+        System.out.println("Ingresa tu primer numero");
+        double num1 = sc.nextDouble();
+
+        //Se ingresará el segundo elemento
+        System.out.println("Ingresa tu segundo numero");
+        double num2 = sc.nextDouble();
+
+        //Se ingresará un tercer elemento
+        System.out.println("Ingresa tu tercer numero");
+        double num3 = sc.nextDouble();
     }
 }
