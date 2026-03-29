37332869cfd34a17366427d11bc0aa1502582ee2
manriquez02cesar <manriquez02cesar@gmail.com>
2026-03-28 23:25:30 -0600

    declare un arreglo para guardar las variables y para poder ordenarlas

--- src/MayorMenor.java
+++ src/MayorMenor.java
@@ -11,9 +11,21 @@ public class MayorMenor {
         //Se ingresará el segundo elemento
         System.out.println("Ingresa tu segundo numero");
         double num2 = sc.nextDouble();
-
+/*
         //Se ingresará un tercer elemento
         System.out.println("Ingresa tu tercer numero");
         double num3 = sc.nextDouble();
+
+ */
+
+        double[] orden = {num1, num2};
+        double cajita = 0;
+
+        if (num2 > num1 | num2 == num1){
+            cajita = orden[0];
+            orden[0] = orden[1];
+            orden[1]= cajita;
+        }
+        System.out.println(orden[0] + ", " + orden[1]);
     }
 }
